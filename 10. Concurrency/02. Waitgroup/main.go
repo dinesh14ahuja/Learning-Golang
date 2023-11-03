944def66211890f5019ccf214287ae6b4eaaffbb
Ahuja <Dinesh.Ahuja@bakerhughes.com>
2023-11-03 16:32:20 +0530

    Waitgroup in go lang

---
+++ 10. Concurrency/02. Waitgroup/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Paramter should have pointer to waitgroup
+func PrintName(s string, wg *sync.WaitGroup) {
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(s)
+		time.Sleep(100)
+	}
+	// Once process is done then we should mark waitgroup as done
+	wg.Done()
+}
+
+func main() {
+
+	// Waitgroup is like an manager which manage the go routines
+
+	var wg sync.WaitGroup
+	// We should add how many go routines will run in parallel
+	wg.Add(2)
+
+	// Go routines should have a parameter to pointer of Waitgroup
+	go PrintName("Dinesh", &wg)
+	go PrintName("Gaurav", &wg)
+
+	// In the end we can call waitgroup.wait . This function will wait until all go routines are done
+	wg.Wait()
+	fmt.Println("Main thread")
+}
